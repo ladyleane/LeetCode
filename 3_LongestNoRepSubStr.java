8a1faa0f7501aea30daf40982c6ca6cf50d2d668
qu4rkb17 <qu4rkb17@gmail.com>
2019-10-30 10:56:13 +0800

    Modify LongestNoRepSubStr, Add Sliding Window

--- 3_LongestNoRepSubStr.java
+++ 3_LongestNoRepSubStr.java
@@ -1,13 +1,14 @@
+// Brute Force, O(n^3)
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+	public int lengthOfLongestSubstring(String s) {
         int n = s.length();
         int ans = 0;
         for(int i = 0; i < n; i++)
             for(int j = i+1; j <= n; j++)
                 if(UniqueChar(s, i, j)) ans = Math.max(ans, j-i);
-        return ans;        
+        return ans;
     }
-    
+
     public boolean UniqueChar(String s, int start, int end){
         Set<Character> set = new HashSet<>();
         for(int i = start; i < end; i++){
@@ -17,4 +18,48 @@ class Solution {
         }
         return true;
     }
+}
+
+
+//Sliding Window, O(2n)
+class Solution{
+	public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+		Set<Character> set = new HashSet<>();
+		int ans = 0, i = 0, j = 0;
+		while (i < n && j < n){
+			//Extend [i, j]
+			if (!set.contains(s.charAt(j))){
+				set.add(s.charAt(j++));
+				ans = Math.max(ans, j - i);
+			}
+			else{
+				set.remove(s.charAt(i++));
+			}
+		}
+		return ans;
+    }
+}
+
+
+//Slinding Window Optimized, O(n)
+class Solution{
+	public int lengthOfLongestSubstring(String s){
+		int n = s.length(), ans = 0;
+		//Create Map window[i, j]
+		Map<Character, Integer> map = new HashMap<>();
+		//Extend [i, j]
+		for (int j = 0, i = 0; j < n; j++){
+			//If window contains current char
+			if (Map.containsKey(s.charAt(j))){
+				//Move left boundary to max(next location of repeating char, i), preventing i from moving to left
+				i = Math.max(map.get(s.charAt(j)), i);
+			}
+			//Why j-i+1? j is the non-repeat index, length of [i, j]
+			ans = Math.max(ans, j - i + 1);
+			
+			map.put(s.charAt(j), j + 1);
+		}
+		return ans;
+	}
 }
