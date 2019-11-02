3d3eb4e893d51aca10ac0bb4f6d56eac1d6b27d2
qu4rkb17 <qu4rkb17@gmail.com>
2019-11-02 15:02:53 +0800

    Add PalindromeNum

---
+++ 9_PalindromeNum.java
@@ -0,0 +1,19 @@
+//Mirror from center
+//without String: % and /	reverse Integer
+//? overflows
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if ( x < 0 || (x % 10 == 0 && x != 0)){
+			return false;
+		}
+		int rev = 0;
+
+		while(x > rev){
+			rev = rev * 10 + x % 10;
+			x /= 10;
+		}
+
+		return x == rev || x == rev / 10;
+    }
+}
