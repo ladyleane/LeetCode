3e9fb5051661379f30be5563a38821f535c9aa48
qu4rkb17 <qu4rkb17@gmail.com>
2019-11-04 10:28:26 +0800

    Add ThreeSumClosest

---
+++ 16_ThreeSumClosest.java
@@ -0,0 +1,24 @@
+//Double Pointer, O(N^2)
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+    	Arrays.sort(nums);
+		int ans = nums[0] + nums[1] + nums[2];
+		for (int i = 0; i < nums.length; i++){
+			int start = i + 1, end = nums.length - 1;
+			//困困困
+			while (start < end){
+				int sum = nums[start] + nums[end] + nums[i];
+				if (Math.abs(target - sum) < Math.abs(target - ans))
+					ans = sum;
+				if (sum > target)
+					end--;
+				else if (sum < target)
+					start++;
+				else
+					return ans;
+			}
+		}
+		return ans;
+    }
+}
