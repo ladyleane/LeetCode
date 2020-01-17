396acd556d72bbb67d242291a80e5a611f821da4
qu4rkb17 <qu4rkb17@gmail.com>
2020-01-17 16:11:02 +0800

    Permutations II

---
+++ 47_PermutationsII.java
@@ -0,0 +1,44 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        Arrays.sort(nums);
+
+        boolean[] used = new boolean[len];
+        Deque<Integer> path = new ArrayDeque<>(len);
+        dfs(nums, len, 0, used, path, res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int len, int depth, boolean[] used, Deque<Integer> stack, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<>(stack));
+            return;
+        }
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) {
+                    continue;
+                }
+
+                used[i] = true;
+                stack.addLast(nums[i]);
+                dfs(nums, len, depth + 1, used, stack, res);
+
+                stack.removeLast();
+                used[i] = false;
+            }
+        }
+    }
+}
