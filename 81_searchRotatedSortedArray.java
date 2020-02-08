35dea91a5799666db815f880b773ab649c95958e
qu4rkb17 <qu4rkb17@gmail.com>
2020-02-08 21:36:43 +0800

    search in rotated sorted array, bi

---
+++ 81_searchRotatedSortedArray.java
@@ -0,0 +1,34 @@
+class Solution{
+	public boolean search(int[] nums, int target){
+		if (nums == null || nums.length == 0){
+			return false;
+		}
+		int start = 0;
+		int end = nums.length - 1;
+		int mid;
+		while (start <= end){
+			mid = start + (end - start) / 2;
+			if (nums[mid] == target){
+				return true;
+			}
+			if (nums[start] == nums[mid]){
+				start++;
+				continue;
+			}
+			if (nums[start] < nums[mid]){
+				if (nums[mid] > target && nums[start] <= target){
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
+			} else {
+				if (nums[mid] < target && nums[end] >= target){
+					start = mid + 1;
+				} else {
+					end = mid - 1;
+				}
+			}
+		}
+		return false;
+	}
+}
