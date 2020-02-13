486209d495015a8dff760ac6a80ce827264eb6fc
qu4rkb17 <qu4rkb17@gmail.com>
2020-02-13 10:13:55 +0800

    graycode

---
+++ 89_graycode.java
@@ -0,0 +1,11 @@
+class Solution{
+	public List<Integer> graycode(int n){
+		List<Integer> res = new ArrayList<Integer>() { {add(0);} };
+		int head = 1;
+		for (int i = 0; i < n; i++){
+			for (int j = res.size() - 1; j >= 0; j--)
+				res.add(head + res.get(j));
+		head <<= 1;
+		}
+	}
+}
