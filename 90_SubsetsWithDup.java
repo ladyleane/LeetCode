fd7930655bd6d4ea9acddc293a5a2ca4a740b7ab
qu4rkb17 <qu4rkb17@gmail.com>
2020-02-14 14:34:25 +0800

    subsets with dup

---
+++ 90_SubsetsWithDup.java
@@ -0,0 +1,18 @@
+public List<List<Integer>> subsetsWithDup(int[] nums){
+	List<List<Integer>> ans = new ArrayList<>();
+	Arrays.sort(nums);
+	getAns(nums, 0, new ArrayList<>(), ans);
+	return ans;
+}
+
+private void getAns(int[] nums, int start, ArrayList<Integer> temp, List<List<Integer>> ans){
+	ans.add(new ArrayList<>(temp));
+	for (int i = start; i < nums.length; i++){
+		if (i > start && nums[i] == nums[i - 1]){
+			continue;
+		}
+		temp.add(nums[i]);
+		getAns(nums, i + 1, temp, ans);
+		temp.remove(temp.size() - 1);
+	}
+}
